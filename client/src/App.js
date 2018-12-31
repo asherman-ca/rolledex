9199e3f6d87e68e14d25b87498b16ceceef7ff2c
Alex Sherman <alex@Alexs-MacBook-Pro.local>
2018-12-30 21:43:16 -0800

    store, router and provider wired

--- client/src/App.js
+++ client/src/App.js
@@ -2,22 +2,28 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import jwt_decode from 'jwt-decode';
 import setAuthToken from './utils/setAuthToken';
 import './App.css';
 
-import Register from './components/layout/Register';
+import store from './store.js';
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <Navbar />
-        <div className="signup-page">
-          <Register />
-        </div>
-      </div>
+      <Provider store={store}>
+        <Router>
+          <div>
+            <Navbar />
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+          </div>
+        </Router>
+      </Provider>
     );
   }
 }
