cb2bff376787ede6a2c542db74bd9f0e790ef50c
Alex Sherman <alex.sherman@SF-MBP-ASHERMAN.local>
2019-03-27 14:42:47 -0700

    initial deployment configs

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
-module.exports = {
-  mongoURI: 'mongodb://dbuser:dbuserpassw0rd@ds037498.mlab.com:37498/rolledex',
-  secretOrKey: 'secret'
-};
+if(process.env.NODE_ENV === 'production'){
+  module.exports = require('./keys_prod')
+} else {
+  module.exports = require('./keys_dev')
+}
